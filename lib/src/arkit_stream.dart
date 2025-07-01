55b7a87f7021b5a60f5cf9899c87816df52b40f6
Shreeshant Prajapati <shreeshant@shrigsolutions.com>
2025-07-01 09:14:30 +0545

    added stream inside arkit

---
+++ lib/src/arkit_stream.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+import 'dart:typed_data';
+import 'package:arkit_plugin/src/models/ar_frame.dart';
+import 'package:flutter/services.dart';
+
+class ARKitCameraStream {
+  static const EventChannel _cameraStreamChannel =
+      EventChannel('arkit/cameraStream');
+
+  static bool get isStreamActive => _subscription != null;
+
+  static Stream<ARFrameData> get cameraImages {
+    return _cameraStreamChannel.receiveBroadcastStream().map((event) {
+      final data = ARFrameData.fromMap(event);
+
+      return data;
+    });
+  }
+
+  static StreamSubscription<ARFrameData>? _subscription;
+
+  static void listen({
+    required void Function(ARFrameData data) onData,
+    void Function(Object error)? onError,
+  }) {
+    _subscription?.cancel();
+    _subscription = cameraImages.listen(
+      onData,
+      onError: onError,
+    );
+  }
+
+  static void dispose() {
+    _subscription?.cancel();
+    _subscription = null;
+  }
+}
